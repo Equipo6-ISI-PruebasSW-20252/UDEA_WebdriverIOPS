c15620fd2ccbf5b98486b563922ccc65447491af
Paulina <125917574+PaulinaG27@users.noreply.github.com>
2025-11-15 20:23:43 -0500

    Create transfer.page.js

---
+++ features/pageobjects/transfer.page.js
@@ -0,0 +1,44 @@
+import Page from './page.js';
+
+/**
+ * Page Object for the Transfer Funds page in ParaBank
+ */
+class TransferFundsPage extends Page {
+    /**
+     * Selectors
+     */
+    get inputAmount() {
+        return $("//input[@id='amount']");
+    }
+
+    get selectFromAccount() {
+        return $("//select[@id='fromAccountId']");
+    }
+
+    get selectToAccount() {
+        return $("//select[@id='toAccountId']");
+    }
+
+    get btnTransfer() {
+        return $("//input[@value='Transfer']");
+    }
+
+    /**
+     * Method
+     */
+    async transfer(amount, originAccount, destinationAccount) {
+        await this.inputAmount.setValue(amount);
+        await this.selectFromAccount.selectByAttribute('value', originAccount);
+        await this.selectToAccount.selectByAttribute('value', destinationAccount);
+        await this.btnTransfer.click();
+    }
+
+    /**
+     * Open Transfer Funds page
+     */
+    open() {
+        return super.open('transfer');
+    }
+}
+
+export default new TransferFundsPage();
